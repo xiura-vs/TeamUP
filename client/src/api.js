073d545175e7e9e4452225659667c1c9a5591fcf
Yug Trivedi <yugt5223@gmail.com>
2026-03-15 11:07:23 +0530

    Rechanged hackathon url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://teamup-jdzz.onrender.com',
+  baseURL: 'https://teamup-jdzz.onrender.com/api',
 });
 
 // Auto-attach JWT token
